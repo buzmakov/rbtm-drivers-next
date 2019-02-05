21e69c3b57d03bf7fed8b747bee0e32de61d1caa
Alexey Buzmakov <buzmakov@gmail.com>
2019-02-05 17:15:03 +0300

    development and angles turning

--- drivers/Detector/Detector.py
+++ drivers/Detector/Detector.py
@@ -16,15 +16,21 @@ class HWDetector(object):
         try:
             self.cam.disable_aeag()
             self.cam.disable_auto_wb()
-            self.cam.enable_recent_frame()
-            self.cam.set_cooling(xidefs.XI_PRM_COOLING)
-
-            print(self.cam.get_cooling())
-            print(self.cam.get_gain())
-            print(self.cam.get_exposure())
+            self.cam.set_gain(0.0)
+            # self.cam.enable_recent_frame()
+            self.cam.set_cooling("XI_TEMP_CTRL_MODE_AUTO")
+            self.cam.set_target_temp(5.0)
+            self.cam.set_imgdataformat("XI_MONO16")
+            # logging.info(self.cam.get_cooling())
+            # # logging.info(self.cam.get_gain())
+            # # logging.info(self.cam.get_exposure_minimum())
+            # logging.info(self.cam.get_target_temp())
+            # logging.info(self.cam.get_temp_selector())
+            logging.info(self.cam.get_acq_frame_burst_count())
 
         except xiapi.Xi_error as err:
             logging.error("Detector.init() failed " + str(err))
+
 
 
     def close(self):
@@ -32,3 +38,9 @@ class HWDetector(object):
             self.cam.close_device()
         except xiapi.Xi_error as err:
             logging.error("Detector.close() failed " + str(err))
+    
+    def get_sensor_temp(self):
+        return self.cam.get_chip_temp()
+    
+    def get_hous_temp(self):
+        return self.cam.get_hous_temp()
--- drivers/tests/test_detector.py
+++ drivers/tests/test_detector.py
@@ -3,5 +3,3 @@ from ..Detector.Detector import HWDetector
 
 def test_detetector():
     d = HWDetector()
-    d.open()
-    d.close()
